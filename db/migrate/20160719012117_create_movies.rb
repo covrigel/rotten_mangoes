e0c9d2e67a732a35ddfe7bb68081b987211593e2
covrigel <alexandra.nemes1986@gmail.com>
2016-07-21 00:54:39 +0000

    part one

---
+++ db/migrate/20160719012117_create_movies.rb
@@ -0,0 +1,15 @@
+class CreateMovies < ActiveRecord::Migration[5.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :director
+      t.string :runtime_in_minutes
+      t.string :integer
+      t.text :description
+      t.string :poster_image_url
+      t.datetime :release_date
+
+      t.timestamps
+    end
+  end
+end
